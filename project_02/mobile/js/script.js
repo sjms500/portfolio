94bdaf2e42c7b637f5add625039e13180f4a91af
sjms500 <sjms500@naver.com>
2022-01-18 17:52:08 +0900

    file8

--- project_02/mobile/js/script.js
+++ project_02/mobile/js/script.js
@@ -4,17 +4,17 @@ var splash=$('.splash-main')
   });
 
 //slide
-// var slide=$('#slide>ul>li');
-// var current=0;
+var slide=$('#slide>ul>li');
+var current=0;
 
-// setInterval(function(){
-//     var prev=slide.eq(current);
-//     current++;
-//     move(prev, 0, '100%');
-//     if(current==slide.size()){current=0;}
-//     var next=slide.eq(current);
-//     move(next, '-100%', 0);
-// },3000);
-// function move(tg, start, end){
-//     tg.css('right', start).stop().animate({right:end},1000);
-// }
+setInterval(function(){
+    var prev=slide.eq(current);
+    current++;
+    move(prev, 0, '-100%');
+    if(current==slide.length){current=0;}
+    var next=slide.eq(current);
+    move(next, '100%', 0);
+},3000);
+function move(tg, start, end){
+    tg.css('left', start).stop().animate({left:end},500);
+}
